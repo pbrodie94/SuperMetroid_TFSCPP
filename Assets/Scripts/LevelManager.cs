543c411666b95fee74ee5ac45c23e6e4fab023c7
pbrodie94 <82655776+pbrodie94@users.noreply.github.com>
2021-06-04 19:50:48 -0400

    Revert "Revert"
    This reverts commit 974bd1a665b9ac33a0dc07add421928e0fd20d60.

--- Assets/Scripts/LevelManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LevelManager : MonoBehaviour
-{
-    public const int MainMenu = 0;
-    public const int Level = 1;
-}
