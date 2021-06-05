e0d976fce456a80f1146b163782e8a769de2c258
pbrodie94 <82655776+pbrodie94@users.noreply.github.com>
2021-06-04 20:14:41 -0400

    Revert

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -86,6 +86,150 @@ public class HUDManager : MonoBehaviour
                 screenFade.color = fadeColour;
             }
         }
+<<<<<<< HEAD:Assets/Scripts/HUDManager.cs
+=======
+
+        if (paused)
+        {
+            PauseMenu();
+        }
+
+        //Checks for pause button
+        if (Input.GetButtonDown(InputManager.pause))
+        {
+            Pause();
+        }
+    }
+
+    private void PauseMenu()
+    {
+        //Gets input for menu selection
+        if (Input.GetButtonDown(InputManager.select) && Input.GetAxisRaw(InputManager.select) > 0)
+        {
+            //Changes the menu selection index
+            pauseMenuSelection--;
+
+            //Loops the menu if the selection goes out of bounds
+            if (pauseMenuSelection < 0)
+            {
+                pauseMenuSelection = pauseMenuItems.Length - 1;
+            }
+
+        } else if (Input.GetButtonDown(InputManager.select) && Input.GetAxisRaw(InputManager.select) < 0)
+        {
+            //Changes the menu selection index
+            pauseMenuSelection++;
+
+            //Loops the menu if the selection goes out of bounds
+            if (pauseMenuSelection > pauseMenuItems.Length - 1)
+            {
+                pauseMenuSelection = 0;
+            }
+        }
+
+        //Sets all menu items to unselected colour
+        for (int i = 0; i < pauseMenuItems.Length; i++)
+        {
+            pauseMenuItems[i].color = defaultColor;
+        }
+
+        //Sets the selected menu item to the selected colour
+        pauseMenuItems[pauseMenuSelection].color = selectedColor;
+
+        //Selects the selected menu item on button press
+        if (Input.GetButtonDown(InputManager.submit))
+        {
+            PauseMenuSelection(pauseMenuSelection);
+        }
+    }
+
+    private void Pause()
+    {
+        //Pauses and unpauses
+        if (paused)
+        { 
+            //Sets pause menu invisible
+            pauseMenu.SetActive(false);
+
+            //Turns off the screen fade
+            Color c = screenFade.color;
+            c.a = 0;
+            screenFade.color = c;
+
+            //Resets timescale to normal speed
+            Time.timeScale = 1;
+
+            paused = false;
+        } else
+        {
+            //Shows pause menu
+            pauseMenu.SetActive(true);
+
+            //Sets the screen tint
+            Color c = screenFade.color;
+            c.a = 0.5f;
+            screenFade.color = c;
+
+            //Ensures the pause menu starts at the top
+            pauseMenuSelection = 0;
+
+            //Sets all menu items to unselected
+            for (int i = 0; i < pauseMenuItems.Length; i++)
+            {
+                pauseMenuItems[i].color = defaultColor;
+            }
+
+            //Sets the selected menu item to selected colour
+            pauseMenuItems[pauseMenuSelection].color = selectedColor;
+
+            //Stops time
+            Time.timeScale = 0;
+
+            paused = true;
+        }
+    }
+
+    private void PauseMenuSelection(int selection)
+    {
+        //Directs pause menu selections
+        switch (selection)
+        {
+            case 0:
+                //Resume
+
+                Pause();
+
+                break;
+
+            case 1:
+                //Restart Level
+
+                Pause();
+                gm.RestartLevel();
+
+                break;
+
+            //case 2:
+                //Options
+
+                 //break;
+
+            case 2:
+                //Quit to Main Menu
+
+                Pause();
+                gm.LoadLevel(LevelManager.MainMenu);
+
+                break;
+
+            case 3:
+                //Quit to Desktop
+
+                Application.Quit();
+
+                break;
+        }
+>>>>>>> parent of acc58ea (Options Menu + Mouse Clicks):Assets/Scripts/UI/HUDManager.cs
     }
 
     public void InitializeEnergy(int energy, int energyTanks, int maxEnergyTanks)
